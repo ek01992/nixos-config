8bca9c200a2a9ff952ff2e7cfac88bc8cdfcd763
Nix OS <root@nixos.com>
2025-05-25 19:44:39 +0000

    push

--- configuration.nix
+++ configuration.nix
@@ -13,7 +13,7 @@
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-
+  boot.supportedFilesystems = [ "btrfs" ];
   networking.hostName = "nixos"; # Define your hostname.
   # Pick only one of the below networking options.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
