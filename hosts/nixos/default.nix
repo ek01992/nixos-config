87b86667ef1592b39dc510d80f69e9b3f474f308
Erik Kowald <ek01992@proton.me>
2025-05-26 00:45:04 -0500

    Add NixOS configuration files: introduce common.nix for shared settings, enhance default.nix with a generator for machine-specific configurations, and add impermanence.nix for persistent directories and files management.

--- hosts/nixos/default.nix
+++ hosts/nixos/default.nix
@@ -1,9 +1,49 @@
-{ lib, ... }:
-with lib;
+{ inputs, self, ... }:
+let
+  # default opts structure
+  dopts = {
+    username = "";
+    userEmail = "";
+    round = false;
+    hostName = "";
+  };
+  nixosGenerator =
+    machine: system: opts:
+    let
+      path = # This will resolve to machine.nix if file is there else machine
+        # Will help to include a dir with multiple files
+        if builtins.pathExists ./${machine + ".nix"} then ./${machine + ".nix"} else ./${machine};
+    in
+    inputs.nixpkgs.lib.nixosSystem {
+      specialArgs = {
+        inherit inputs self;
+        opts = dopts // opts // { hostName = machine; };
+      };
+      modules = [
+        ./common.nix # nixos Common settings
+
+        # setting the machine system type
+        {
+          nixpkgs.hostPlatform = system;
+        }
+
+        # machine specific settings tweaks
+        # If the machine specific settings are grwoing much then create a dir
+        # default.nix inside the dir will be imported here
+        path
+      ];
+    };
+in
 {
-  imports =
-    with builtins;
-    map (fn: ./${fn}) (
-      filter (fn: (fn != "default.nix" && !hasSuffix ".md" "${fn}")) (attrNames (readDir ./.))
-    );
+  flake.nixosConfigurations = {
+    vm = nixosGenerator "vm" "x86_64-linux" {
+      username = "vm";
+      userEmail = "vm@localhost";
+    };
+    mach = nixosGenerator "mach" "x86_64-linux" {
+      username = "erik";
+      userEmail = "ek01992@proton.me";
+      rounding = true;
+    };
+  };
 }
