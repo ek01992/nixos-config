9b0814285e60f00a2f96517234ebe36703f1b81f
Erik Kowald <ek01992@proton.me>
2025-05-28 20:47:05 -0500

    Remove obsolete configuration files and settings, including .envrc, .gitignore, flake.lock, and various Nix modules, to streamline the project structure and enhance maintainability.

--- modules/default.nix
+++
@@ -1,6 +0,0 @@
-{
-  flake = {
-    homeModules = import ./home;
-    nixosModules = import ./nixos;
-  };
-}
--- modules/home/default.nix
+++
@@ -1,7 +0,0 @@
-{
-  # Shell module is responsible for setting up the cli environment
-  # This is the environment that i am comfortable with
-  nix = ./nix;
-  shell = ./shell;
-  home = ./home.nix;
-}
