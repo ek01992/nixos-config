8aee5df27569ffb58a467f358a61dbef3b2f4be9
Erik Kowald <ek01992@proton.me>
2025-06-03 23:32:19 -0500

    Add user configuration for 'erik' in default.nix, including group assignments, SSH keys, and home-manager integration, while removing previous user settings from xps/default.nix for improved organization and clarity.

---
+++ hosts/common/users/erik/default.nix
@@ -0,0 +1,42 @@
+{
+  pkgs,
+  config,
+  ...
+}: let
+  ifTheyExist = groups:
+    builtins.filter (group: builtins.hasAttr group config.users.groups) groups;
+in {
+  users.mutableUsers = false;
+  users.groups.erik.gid = 1000;
+  users.users.erik = {
+    uid = 1000;
+    isNormalUser = true;
+    shell = pkgs.bash;
+    group = "erik";
+    extraGroups =
+      [
+        "wheel"
+        "users"
+        "audio"
+        "video"
+      ]
+      ++ ifTheyExist [
+        "adbusers"
+        "docker"
+        "libvirtd"
+        "minecraft"
+        "mysql"
+        "network"
+        "podman"
+        "wireshark"
+      ];
+
+    openssh.authorizedKeys.keys = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPakom6FvoSpBc0nmunHQUZwQI9VtS52i4W4WLuiUMpc ek01992@proton.me"];
+    initialPassword = "temp";
+    packages = with pkgs; [home-manager];
+  };
+
+  home-manager.users.erik = import ../../../../home/erik/${config.networking.hostName};
+
+  security.pam.services.swaylock = {};
+}
--- hosts/xps/default.nix
+++ hosts/xps/default.nix
@@ -11,31 +11,5 @@
     useDHCP = true;
   };
 
-  users.users.erik = {
-    isNormalUser = true;
-    extraGroups = [ "wheel" ];
-    initialPassword = "temp";
-    createHome = true;
-    home = "/home/erik";
-    openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPakom6FvoSpBc0nmunHQUZwQI9VtS52i4W4WLuiUMpc ek01992@proton.me"
-    ];
-  };
-  users.mutableUsers = false;
-
-  services.openssh = {
-    enable = true;
-    settings = {
-      PermitRootLogin = "prohibit-password";
-      PasswordAuthentication = false;
-    };
-  };
-
-  home-manager.users.erik = {
-    imports = [
-      ../../home/erik/xps
-    ];
-  };
-
   system.stateVersion = "25.11";
 }
