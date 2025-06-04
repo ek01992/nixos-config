ad29b137b83c94e34441e559e94821b26ec21e2e
Erik Kowald <ek01992@proton.me>
2025-06-03 20:42:10 -0500

    Remove unused CLI tools 'myip' and 'urlencode' from default.nix to streamline configuration.

--- home/erik/common/global/cli/default.nix
+++ home/erik/common/global/cli/default.nix
@@ -13,11 +13,9 @@
   home.packages = with pkgs; [
     alejandra # Nix Formatter
     bash-language-server # Bash LSP
-    myip # ip fetcher
     nh # Home manager and Nix wrapper
     nix-search-tv # Fuzzy search for NixOS packages
     nixd # Nix LSP
     prettierd # Common formatter
-    urlencode # URI encoder
   ];
 }
